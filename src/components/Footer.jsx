69c8892cd9414b4f028396db0d238473d59bb2d5
ABUBAKAR-DAHIR <abu112abu112abu112@gmail.com>
2025-08-04 08:36:34 +0530

    width adjusting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,11 +4,13 @@
     export default function Footer() {
     return (
         <div id='footer' className='footer'>
-            <p>© {new Date().getFullYear} ReactLearn. All rights reserved</p>
-            <div className='socials'>
-                <a href=""><i className='fab fa-github'></i></a>
-                <a href=""><i className='fab fa-linkedin'></i></a>
-                <a href=""><i className='fab fa-x'></i></a>
+            <div className="footer-wrapper">
+                <p>© {new Date().getFullYear} ReactLearn. All rights reserved</p>
+                <div className='socials'>
+                    <a href=""><i className='fab fa-github'></i></a>
+                    <a href=""><i className='fab fa-linkedin'></i></a>
+                    <a href=""><i className='fab fa-x'></i></a>
+                </div>
             </div>
         </div>
     )
